7ea5a11084ac02584c451e3e1d25fdd86e92ab60
ENZO ACOSTA <enzoacosta@uc.cl>
2025-01-07 19:00:42 -0300

    ADD base form & base result view

--- main.py
+++ main.py
@@ -11,9 +11,18 @@ def home():
         return render_template("home.html")
 
 
-@app.route("/generate")
+@app.route("/generate", methods=["POST", "GET"])
 def generate():
-    return render_template("generate.html")
+    if request.method == "POST":
+        form_size = request.form["size"]
+        return redirect(url_for("result", size=form_size))
+    else:
+        return render_template("generate.html")
+
+
+@app.route("/result/<size>")
+def result(size):
+    return render_template("result.html", result=size)
 
 
 if __name__ == "__main__":
